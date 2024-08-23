a0ad50f73d4ff9ef5047e91cd4a216652f991541
ioaviator <kingsolomonifeanyi@gmail.com>
2024-08-23 14:16:30 +0100

    launch program from main file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-
+from utils.create_dir import create_download_dir
+from utils.download_file import download_file
+from extract import extract_file
 
 
 if __name__ == "__main__":
-    print('Setting up')
+    data_folder = create_download_dir()
+    file_path, download_dir = download_file(data_folder)
+    extract_file(file_path, download_dir)
