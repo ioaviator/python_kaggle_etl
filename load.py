c79eb8d3736f1df73de92f62ced8b949bd838e74
ioaviator <kingsolomonifeanyi@gmail.com>
2024-08-24 16:56:05 +0100

    install connector for database configuration

--- load.py
+++ load.py
@@ -4,9 +4,9 @@ import pandas as pd
 def load(df):
 
     #connect to database
-    engine = sa.create_engine('mssql+pyodbc://AVIATOR/doby?driver=ODBC+Driver+17+for+SQL+Server')
+    engine = sa.create_engine('mssql+pyodbc://AVIATOR/ecommerce?driver=ODBC+Driver+17+for+SQL+Server')
 
     # load data into database table
-    df.to_sql(name='large_companies_by_revenue', con=engine, index=False, if_exists='replace')
+    df.to_sql(name='orders', con=engine, index=False, if_exists='replace')
 
     return True
