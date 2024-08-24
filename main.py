f023d3d857f7e5339f3a55cf618f387b8f31c1b9
ioaviator <kingsolomonifeanyi@gmail.com>
2024-08-24 16:50:38 +0100

    structure functions in main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from utils.create_dir import create_download_dir
 from utils.download_file import download_file
 from extract import extract_file
+from transform import transform
+from load import load
 
-
-if __name__ == "__main__":
+def main():
     data_folder = create_download_dir()
     file_path, download_dir = download_file(data_folder)
+    
     extract_file(file_path, download_dir)
+
+    data_df = transform()
+
+    load(data_df)
+
+    return True
+
+if __name__ == "__main__":
+    main()
